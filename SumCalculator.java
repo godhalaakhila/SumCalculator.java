f386edc08f889260c230b3338ea8a737f2a6ca73
godhalaakhila <akhilareddyg55@gmail.com>
2025-01-30 18:21:44 +0530

    Add files via upload

---
+++ SumCalculator.java
@@ -0,0 +1,22 @@
+public class SumCalculator {
+
+    // Function to calculate the sum of two numbers
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        // Define two numbers
+        int number1 = 10;
+        int number2 = 20;
+
+        // Call the sum function and store the result
+        int result = sum(number1, number2);
+
+        // Print the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + result);
+    }
+}
+ 
+    
+
